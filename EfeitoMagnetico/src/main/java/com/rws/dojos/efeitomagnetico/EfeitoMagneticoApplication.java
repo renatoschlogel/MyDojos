df3e0ce538de16ce0839d80c6729d0a75db9c7e7
Renato Welinton Schlogel <renato.s@outlook.com>
2021-10-18 00:04:42 -0300

    Estrutura do Projeto

---
+++ EfeitoMagnetico/src/main/java/com/rws/dojos/efeitomagnetico/EfeitoMagneticoApplication.java
@@ -0,0 +1,13 @@
+package com.rws.dojos.efeitomagnetico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfeitoMagneticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfeitoMagneticoApplication.class, args);
+	}
+
+}
---
+++ EfeitoMagnetico/src/test/java/com/rws/dojos/efeitomagnetico/EfeitoMagneticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rws.dojos.efeitomagnetico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfeitoMagneticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
